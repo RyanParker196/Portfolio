564d898b3007fd790a9513b4f343cab9bc77f0b9
RyanParker196 <46787914+RyanParker196@users.noreply.github.com>
2020-03-07 05:01:23 -0500

    major style changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav'
 import Image from 'react-bootstrap/Image'
-import Carousel from './components/Carousel'
+// import Carousel from './components/Carousel'
 import me from "./assets/me.jpg"
 import './App.css'
 
@@ -9,16 +9,10 @@ export default class App extends Component {
   render() {
     return (
       <div className="BigDiv">
-        <div className="Avatar">
-          <Image bg="dark" src={ me } alt="error loading img" roundedCircle />
-        </div>
         <Nav />
-        <header >
-          {/* <h2 className="Greeting">
-            View my recent projects by clicking any of the links below
-          </h2> */}
-        </header>
-        <Carousel />
+        <Image src={ me } alt="error loading img" roundedCircle />
+        {/* <Carousel /> */}
+
       </div>
     );
   }
